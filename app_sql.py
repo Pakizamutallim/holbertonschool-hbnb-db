8b258b72a388ac7b6cc81e52d7d3df3772261404
Pakizamutallim <62bq5tn@code.edu.az>
2024-07-07 21:26:02 +0400

    created dockerfile and changed api file

--- app_sql.py
+++ app_sql.py
@@ -1,5 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import secrets
+
+# Load environment variables from .env file
+load_dotenv()
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
